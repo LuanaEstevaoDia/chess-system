352f2a596464364ab041fe33be37e3b57dcf1650
LuanaEstevaoDia <luanafreitas.ti@gmail.com>
2024-05-16 11:25:27 -0300

    Save: Starting to implement Board and Piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -9,5 +9,22 @@ package boardgame;
  * @author luana
  */
 public class Piece {
+    protected Position position;//posição simples de matriz, não é ainda a posição de xadrez.
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        //A posição não entrou porque a peça inicia como nula.
+    }
+
+    protected Board getBoard() {
+        return board;
+        //Protegido para ser acessado só pelas peças
+    }
+
+    }
     
-}
+    
+    
+    
+
