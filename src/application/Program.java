4dd8c9dc9e651d509bbf114c41182267db5690b0
LuanaEstevaoDia <luanafreitas.ti@gmail.com>
2024-06-03 10:34:11 -0300

    Handling exceptions and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,9 +4,11 @@
  */
 package application;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -14,28 +16,34 @@ import java.util.Scanner;
  * @author luana
  */
 public class Program {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         //Lógica para movimentar as peças
-        while (true) {     
-             UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
-            
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
-            
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        while (true) {
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();//Programa aguardar para apertar enter
+
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();//Programa aguardar para apertar enter
+
+            }
+
         }
-       
-        
-        
-        
-       
-       
     }
-    
 }
